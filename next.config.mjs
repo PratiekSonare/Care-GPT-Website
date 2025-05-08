22807b94a95281e858cbfecd889eca96411150e3
PratiekSonare <pratiksonare16@gmail.com>
2025-05-08 18:36:21 +0530

    export deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export', // For Next.js 13+ with App Router
+  };
+  
 export default nextConfig;
